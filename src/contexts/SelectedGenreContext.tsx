154c3c2270cacf1d4cf3cbd2af442b1180008a7c
viniciusbe <vinicius.bernardesds@gmail.com>
2021-03-23 15:15:20 -0300

    refactor: dele non used state

--- src/contexts/SelectedGenreContext.tsx
+++ src/contexts/SelectedGenreContext.tsx
@@ -10,17 +10,10 @@ interface selectedGenreProviderProps {
     children: ReactNode;
 }
 
-interface GenreResponseProps {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-}
-
 export const SelectedGenreContext = createContext({} as SelectedGenreContextData)
 
 export function SelectedGenreProvider({ children }: selectedGenreProviderProps) {
     const [selectedGenreId, setSelectedGenreId] = useState(1);
-    const [selectedGenre, setSelectedGenre] = useState({} as GenreResponseProps);
 
     function changeGenre(id:number) {
         setSelectedGenreId(id)
